5c360dc5e49bd4faea804a0aa9631b031ec48b97
mizunoryuki <135314422+mizunoryuki@users.noreply.github.com>
2025-03-05 10:34:49 +0900

    dynamic routes

---
+++ src/app/[slug]/page.tsx
@@ -0,0 +1,19 @@
+import Link from "next/link";
+import styles from "./page.module.css";
+
+export default async function Page({
+    params,
+}: {
+    params: Promise<{ slug: string }>;
+}) {
+    const { slug } = await params;
+
+    return (
+        <div className={styles.container}>
+            <div className={styles.page}>This page is /{slug}</div>
+            <Link href={`/`} className={styles.link}>
+                <p>Back page</p>
+            </Link>
+        </div>
+    );
+}
